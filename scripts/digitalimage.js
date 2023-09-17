e26150a93f9899a97b38b2a1f09cccfb80a5351a
digitalimage <145292729+MDigitalImage@users.noreply.github.com>
2023-09-17 22:53:58 +0300

    Add files via upload

---
+++ scripts/digitalimage.js
@@ -0,0 +1,95 @@
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+        if(entry.isIntersecting){
+            entry.target.classList.add("show");
+        }else{
+            entry.target.classList.remove("show");
+        }
+    })
+})
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el)=> observer.observe(el));
+
+const elementHome = document.getElementById("home");
+const elementServices = document.getElementById("services");
+const elementProjects = document.getElementById("projects");
+const elementAbout = document.getElementById("about");
+const elementContact = document.getElementById("contact");
+const elementLogo = document.getElementById("logo");
+const elementLogo2 = document.getElementById("logo2");
+
+const elementArrowtop = document.getElementById("arrowtop")
+
+elementLogo.addEventListener('click', function(){
+    window.open("digitalimage.html", "_self");
+})
+
+elementLogo2.addEventListener('click', function(){
+    window.open("digitalimage.html", "_self");
+})
+
+document.querySelectorAll('.header-text').forEach((headerItem) => {
+    const headerItemText = headerItem.textContent.trim();
+    headerItem.addEventListener('click', function() {
+        if(headerItemText == 'Home' ){
+            elementHome.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+        }else if(headerItemText == 'Services'){
+            elementServices.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+        }else if(headerItemText == 'Projects'){
+            elementProjects.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+        }else if(headerItemText == 'About'){
+            elementAbout.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+        }else if(headerItemText == 'Contact'){
+            elementContact.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+        }
+    })    
+})
+
+const para = document.getElementById('para');
+
+para.addEventListener('scroll',function(){
+    var scrollPercent = Math.round(para.scrollTop );
+    if(scrollPercent>700) {
+        elementArrowtop.style.display="inline-flex";
+    }
+    if(scrollPercent>2800){
+        elementArrowtop.style.bottom="8rem";
+    }
+    if(scrollPercent<700) {
+        elementArrowtop.style.display="none";
+    }
+    if(scrollPercent<2800) {
+        elementArrowtop.style.bottom="2rem";
+    }
+})
+
+
+const menuIcon = document.getElementById('menu'); 
+const floationMenu = document.getElementById('floating-menu');
+
+menuIcon.addEventListener('click', function(){
+    para.style.filter = "blur(0.4rem)"
+    floationMenu.style.display = "flex"
+})
+
+document.querySelectorAll('.header-text2').forEach((headerItem) => {
+    const headerItemText = headerItem.textContent.trim();
+    headerItem.addEventListener('click', function() {
+        para.style.filter = "blur(0rem)"
+        floationMenu.style.display = "none"
+        if(headerItemText == 'Home' ){
+            elementHome.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+        }else if(headerItemText == 'Services'){
+            elementServices.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+        }else if(headerItemText == 'Projects'){
+            elementProjects.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+        }else if(headerItemText == 'About'){
+            elementAbout.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+        }else if(headerItemText == 'Contact'){
+            elementContact.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+        }
+    })    
+})
+
+
