1423a627a64173265f5c6613e4acae086f8776c7
digitalimage <145292729+MDigitalImage@users.noreply.github.com>
2023-09-18 17:50:44 +0300

    Add files via upload

--- scripts/digitalimage.js
+++ scripts/digitalimage.js
@@ -8,8 +8,22 @@ const observer = new IntersectionObserver((entries)=>{
     })
 })
 
+const observer2 = new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+        if(entry.isIntersecting){
+            entry.target.classList.add("show2");
+        }else{
+            entry.target.classList.remove("show2");
+        }
+    })
+})
+
+
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el)=> observer.observe(el));
+
+const hiddenElements2 = document.querySelectorAll('.hidden2');
+hiddenElements2.forEach((el)=> observer2.observe(el));
 
 const elementHome = document.getElementById("home");
 const elementServices = document.getElementById("services");
